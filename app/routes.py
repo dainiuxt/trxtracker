c8efe8e49a74da35deb7588742b5dd9b6d80b8fc
dainiuxt <dainiuxt@gmail.com>
2025-02-19 20:44:07 +0200

    finish login page frontend

--- app/routes.py
+++ app/routes.py
@@ -2,31 +2,39 @@ from flask import render_template
 from app import app
 from app.forms import LoginForm
 
+title = 'TRX Exercises'
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-  exercises = [
-    {
-      'name': 'Rowing',
-      'description': 'A good way to start your day',
-      'image': 'img.jpg',
-      'alt': 'Rowing'},
-    {
-      'name': 'Yoga',
-      'description': 'A good way to start your day',
-      'image': 'img.jpg',
-      'alt': 'Yoga'},
-    {
-      'name': 'Cycling',
-      'description': 'A good way to start your day',
-      'image': 'img.jpg',
-      'alt': 'Cycling'}
-  ]
-  return render_template('index.html',
-                         title='TRX Exercises',
-                         exercises=exercises)
+    exercises = [
+        {
+            'name': 'Rowing',
+            'description': 'A good way to start your day',
+            'image': 'img.jpg',
+            'alt': 'Rowing'},
+        {
+            'name': 'Yoga',
+            'description': 'A good way to start your day',
+            'image': 'img.jpg',
+            'alt': 'Yoga'},
+        {
+            'name': 'Cycling',
+            'description': 'A good way to start your day',
+            'image': 'img.jpg',
+            'alt': 'Cycling'}
+    ]
+    return render_template('index.html',
+                           title=title,
+                           name="Home",
+                           exercises=exercises)
+
 
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)  
+    return render_template('login.html',
+                           title=title,
+                           name='Login',
+                           form=form)
